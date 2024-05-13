ce2bbc7306e052b31817adbdd91b37e95437d4d0
riqsans <thrq88@gmail.com>
2024-05-13 15:30:26 +0700

    menuliskan akses modifier public untuk class mahasiswa

---
+++ Constructor.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa
+{
+public:
+    int nim;
+    string nama;
+
+    Mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+    Mahasiswa(int iNim)
+    {
+        nim = iNim;
+    };
+
+    
+}
