0ec527f32798a2d5bd93444a204ec8ddbb1c237e
riqsans <thrq88@gmail.com>
2024-05-13 15:51:31 +0700

    membuat int main

--- Constructor.cpp
+++ Constructor.cpp
@@ -18,5 +18,37 @@ public:
         nim = iNim;
     };
 
-    
+    Mahasiswa(string);
+    Mahasiswa(int iNim, string iNama);
+    void cetak()
+    {
+        cout << "NIM = " << nim << endl;
+        cout << "Nama = " << nama << endl;
+        cout << endl;
+    }
+};
+
+Mahasiswa::Mahasiswa(string iNama){
+    nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama)
+    {
+        nim = iNim;
+        nama = iNama;
+    };
+
+int main()
+{
+   Mahasiswa mhs1;
+   Mahasiswa mhs2(100);
+   Mahasiswa mhs3("Thoriq");
+   Mahasiswa mhs4(100,"Thoriq");
+
+   mhs1.cetak();
+   mhs2.cetak();
+   mhs3.cetak();
+   mhs4.cetak();
+
+   return 0; 
 }
