0fcef03337c0b2924a8235103b265a8be67149fe
riqsans <thrq88@gmail.com>
2024-05-13 19:07:20 +0700

    menuliskan int main

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -16,3 +16,11 @@ public:
         return this->judul;
     }
 } bukunya;
+
+int main()
+{
+    // bukunya.setJudul("Matematika");
+    // cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
+    return 0;
+}
