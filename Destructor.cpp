fbbfaef52c9904f36c267bab97428fd4997320f9
riqsans <thrq88@gmail.com>
2024-05-13 19:21:02 +0700

    menuliskan int main

--- Destructor.cpp
+++ Destructor.cpp
@@ -38,3 +38,10 @@ void angka::isiData(){
     cout<<endl;
 }
 
+int main(){
+    angka belajarcpp(3); //Constructor Dipanggil
+    angka *ptrBelajarcpp = new angka(5); //Constructor Dipanggil
+    delete ptrBelajarcpp; //Destuctor Dipanggil
+
+    return 0;
+}//Destructor Dipanggil
